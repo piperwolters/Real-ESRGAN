f8384c8085027a778617582332d15153bc239843
Piper Wolters <piperw@prior-cirrascale-74.reviz.ai2.in>
2023-06-30 16:05:15 -0700

    12s2 experiment cfgs and inference on gpu oops

--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -25,8 +25,11 @@ def main():
 
     args = parser.parse_args()
 
+    device = torch.device("cuda")
+
     # model 
     model = RRDBNet(num_in_ch=6, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+    model = model.to(device)
     netscale = 4
 
     # determine model paths
@@ -40,8 +43,6 @@ def main():
 
     input_path = '/data/piperw/inference_data/' + args.input + '/s2_condensed' 
     paths = sorted(glob.glob(os.path.join(input_path, '*/*.png')))
-
-    paths = paths * 100
 
     batches_names, batch_names = [], []  # list of list of [basename, imgname]
     batches = []
@@ -62,7 +63,7 @@ def main():
         batch.append(img)
         batch_names.append([basename, imgname])
         if len(batch) == 64:
-            batches.append(torch.stack(batch))
+            batches.append(torch.stack(batch).to(device))
             batches_names.append(batch_names)
             batch, batch_names = [], []
 
@@ -71,23 +72,20 @@ def main():
         with torch.cuda.amp.autocast(enabled=True):
             for i,batch in enumerate(batches):
                 try:
-                    print("input:", batch.shape, " time:", time.perf_counter())
                     output = model(batch)
-                    print("output:", output.shape, " time:", time.perf_counter())
                     output = torch.permute(output, (0, 2, 3, 1)).squeeze()
                 except RuntimeError as error:
                     print('Error', error)
 
                 else:
-                    print("saving...", time.perf_counter())
                     for j,b in enumerate(output):
                         basename, imgname = batches_names[i][j]
 
                         os.makedirs(basename, exist_ok=True)
                         save_path = os.path.join(basename, f'{imgname}.{extension}')
 
-                        #skimage.io.imsave(save_path, b.detach().numpy())
-                    print("done saving batch...", time.perf_counter())
+                        print("saving...", save_path)
+                        skimage.io.imsave(save_path, b.detach().numpy())
 
 
 if __name__ == '__main__':
